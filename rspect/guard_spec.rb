226b0d8e9af30a436af46274095face3d4ed1bf4
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-06 01:53:58 -0600

    Adding example with rspect

---
+++ rspect/guard_spec.rb
@@ -0,0 +1,10 @@
+
+RSpec.describe Guard do
+  # Description
+  describe '#can_participate_in_lottery?' do
+    #Test Body
+    it 'returns true if user age is greater than 17' do
+
+    end
+  end
+end
