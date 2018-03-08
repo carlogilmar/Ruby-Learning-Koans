fc5f2926952c0a5b1523636565a673d9e5708280
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-07 23:46:13 -0600

    Adding progress

--- rspect/guard_spec.rb
+++ rspect/guard_spec.rb
@@ -1,10 +1,20 @@
+require './guard'
 
 RSpec.describe Guard do
   # Description
   describe '#can_participate_in_lottery?' do
+
     #Test Body
     it 'returns true if user age is greater than 17' do
-
+      service = Guard.new
+      expect(service.can_participate_in_lottery?(age:20)).to eq(true)
     end
+
+    it 'returns false if user age is not greater tahn 17' do
+      service = Guard.new
+      expect( service.can_participate_in_lottery?(age:16) ).to eq(false)
+    end
+
+
   end
 end
